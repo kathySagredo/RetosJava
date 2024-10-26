cee99b83502038172dc7b198f7f576e4833cf889
Kathy Sagredo <sagredo.kathy@gmail.com>
2024-10-25 23:34:37 -0300

    Reto: Estaciones mejorada con Switch

--- src/Estaciones.java
+++ src/Estaciones.java
@@ -5,17 +5,27 @@ public class Estaciones {
         System.out.println("*** Estaciones del año ***");
         System.out.print("Número del mes: ");
         var n_mes = new Scanner(System.in).nextInt();
-        String s_mes;
 
-        if(n_mes == 1 || n_mes == 2 || n_mes == 12){
-            s_mes = "Verano";
-        } else if (n_mes >= 3 && n_mes <= 5) {
-            s_mes = "Otoño";
-        } else if (n_mes >=6 && n_mes <= 8) {
-            s_mes = "Invierno";
-        } else {
-            s_mes ="Primavera";
-        }
+        var s_mes = switch (n_mes){
+            case 12, 1,2 -> "Verano";
+            case 3,4,5 -> "Otoño";
+            case 6,7, 8 -> "Invierno";
+            case 9,10,11 -> "Primavera";
+            default -> "Estación desconocida";
+        };
+
+//
+//        String s_mes;
+//
+//        if(n_mes == 1 || n_mes == 2 || n_mes == 12){
+//            s_mes = "Verano";
+//        } else if (n_mes >= 3 && n_mes <= 5) {
+//            s_mes = "Otoño";
+//        } else if (n_mes >=6 && n_mes <= 8) {
+//            s_mes = "Invierno";
+//        } else {
+//            s_mes ="Primavera";
+//        }
 
         System.out.println("Estación: " + s_mes);
     }
